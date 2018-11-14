46171f68abda5dfa212625b10cf20765d466f7d0
Janus Olsen <janus-olsen91@hotmail.com>
2018-11-14 15:06:05 +0100

    Initial Commit

---
+++ FlexicuV2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FlexicuV2
+//
+//  Created by Janus Olsen on 14/11/2018.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FlexicuV2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FlexicuV2
+//
+//  Created by Janus Olsen on 14/11/2018.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
