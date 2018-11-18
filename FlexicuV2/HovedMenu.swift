c4f31544d8533fe77020e294252bb2e22227eac9
Janus Olsen <janus-olsen91@hotmail.com>
2018-11-18 11:36:53 +0100

    Virkende funktionalitet på "hjem" skærmen - mangler dog at påføre de rigtige data. Der er også oprettet enkelte "modeller"

--- FlexicuV2/HovedMenu.swift
+++ FlexicuV2/HovedMenu.swift
@@ -8,28 +8,65 @@
 
 import UIKit
 
-class HovedMenu: UIViewController {
-
+class HovedMenu: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    @IBOutlet weak var CollectionView1: UICollectionView! //Udlejede
+    
+    @IBOutlet weak var CollectionView2: UICollectionView! // Lejede
+    
+    @IBOutlet weak var CollectionView3: UICollectionView! // Alle ens medarbejdere
+    
+    let udlejedeMIdentifier = "udlejedeMCell"
+    let lejetACVIdentifier = "lejetArbejdskraftCell"
+    let alleMCVIdentifier = "alleMedarbejdereCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+
+        
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10 // returner længden på datasættene
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == CollectionView1 {
+        let cell: UdlejedeMedarbejdereCollectionViewCell? = collectionView.dequeueReusableCell(withReuseIdentifier: udlejedeMIdentifier, for: indexPath) as? UdlejedeMedarbejdereCollectionViewCell
+            cell?.navnLabel.text = "Init All"
+        return cell!
+        } else if collectionView == CollectionView2{
+            let cell: LejetArbejdskraftCVCell? = collectionView.dequeueReusableCell(withReuseIdentifier: lejetACVIdentifier, for: indexPath) as? LejetArbejdskraftCVCell
+            cell?.navnLabel.text = "Hej hej"
+            return cell!
+        } else{
+            let cell: AlleMedarbejdereCVCell? = collectionView.dequeueReusableCell(withReuseIdentifier: alleMCVIdentifier, for: indexPath) as? AlleMedarbejdereCVCell
+            cell?.navnLabel.text = "Hej hej"
+            cell?.lejetAfLabel.text = "Janus rules"
+            return cell!
+            
+        }
+        
+        
+    }
+    
 
+
+
+
+
+        
 }
+    
+    
+
